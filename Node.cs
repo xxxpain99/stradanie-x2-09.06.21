e139768ff682c62607c876f81ddf826a24a703f6
xxxpain99 <84181647+xxxpain99@users.noreply.github.com>
2021-06-09 13:28:17 +0500

    Add files via upload

---
+++ Node.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Node <T>
+	{
+		public T value;
+		public Node<T> next;
+		public Node<T> prev;
+		public int index;
+
+		public void SetIndex( int index)
+		{
+			this.index = index;
+		}
+		public static int Count = 0;
+		public Node(T value)
+		{
+			this.value = value;
+		}
+
+		public Node()
+		{
+
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyList<int> ert = new MyList<int>();
+			ert.Add(45);
+			ert.Add(55);
+			ert.Add(65);
+			ert.AddFront(89);
+			ert.Print();
+			ert.PushBack(10); //dobavlyaem element v nachale spiska
+			ert.PushFront(50);
+
+			Console.WriteLine();
+			ert.Print();
+			Console.WriteLine("Perviy element spiska");
+			Console.WriteLine(ert.Get(1));
+			Console.WriteLine();
+
+			Console.WriteLine();
+			ert.Print();
+			Console.WriteLine("Posledniy element spiska");
+			Console.WriteLine(ert.Get(4));
+			Console.WriteLine();
+
+			Console.WriteLine();
+			ert.Print();
+			Console.WriteLine("Spisok v obratnom poryadke");
+			Console.WriteLine(50);
+		}
+	}
+}
